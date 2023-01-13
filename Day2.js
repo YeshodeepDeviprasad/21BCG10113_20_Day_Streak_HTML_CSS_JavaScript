4baa462f1683e56357893bb7470afd8b23680eaa
YeshodeepDeviprasad <119286775+YeshodeepDeviprasad@users.noreply.github.com>
2023-01-13 22:17:46 +0530

    Delete Day2.js

--- Day2.js
+++
@@ -1,6 +0,0 @@
-function add() {
-    var num1 = document.getElementById("num1").value;
-    var num2 = document.getElementById("num2").value;
-    var result = document.getElementById("result");
-    result.innerHTML = "The result is: " + (parseFloat(num1) + parseFloat(num2));
-  }
