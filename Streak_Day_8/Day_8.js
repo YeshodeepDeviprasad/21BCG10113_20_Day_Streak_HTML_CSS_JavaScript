47d36a6c880ea130e781b62eb253d2a9be2975f5
Yeshodeep Dohre <yeshodeep.deviprasad2021@vitbhopal.ac.in>
2023-01-21 17:22:43 +0530

    This code create a * pattern in a triangle shape.

---
+++ Streak_Day_8/Day_8.js
@@ -0,0 +1,9 @@
+var triangle = "";
+for (var i = 1; i <= 5; i++) {
+  for (var j = 1; j <= i; j++) {
+    triangle += "*";
+  }
+  triangle += "<br>";
+}
+document.getElementById("triangle").innerHTML = triangle;
+
